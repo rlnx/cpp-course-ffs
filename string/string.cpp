3ee885a26e71a1601637ad3d597c6486893d48fc
Ruslan Israfilov <ruslan.is@outlook.com>
2018-10-08 23:55:47 +0300

    Fix wordings, add comments, cleanup the code

--- string/string.cpp
+++ string/string.cpp
@@ -2,13 +2,13 @@
 
 class string {
 public:
-
+  /* Empty constructor */
   string() {
     size_ = 0;
     str_ = nullptr;
   }
 
-  /* Constructor */
+  /* Constructor from C-style string */
   string(const char *source) {
     size_ = string_length(source);
     str_ = copy_string(source, size_);
@@ -22,25 +22,27 @@ public:
     std::cout << "[DEBUG]: Copy constructor called" << std::endl;
   }
 
+  /* Destructor */
   ~string() {
     delete[] str_;
     size_ = 0;
     std::cout << "[DEBUG]: Destructor called" << std::endl;
   }
 
-  /* Asigment operator */
+  /* Assignment operator */
   string &operator=(const string &other) {
-    /* Self asigment check */
-    /* Avoid self asigment!!! */
+    /* Self assignment check */
+    /* Avoid self assignment!!! */
     if (this != &other) {
       delete[] this->str_;
       this->size_ = other.size_;
       this->str_ = copy_string(other.str_, size_);
     }
-    std::cout << "[DEBUG]: Asigment operator" << std::endl;
+    std::cout << "[DEBUG]: Assignment operator" << std::endl;
     return *this;
   }
 
+  /* Concatenation operator (+ operator overloading) */
   string operator+(const string &other) {
     char *concat = new char[size_ + other.size_ + 1];
     for (size_t i = 0; i < size_; i++) {
@@ -62,15 +64,20 @@ public:
   }
 
 private:
+  /* Computes length of the C-style string (excluding null-character) */
   size_t string_length(const char *source) {
     size_t i = 0;
+    /* Perform iterations until null-character is found */
     while (source[i] != '\0') {
       i++;
     }
     return i;
   }
 
+  /* Allocates array of the given length and copies the given
+   * C-style string to that array, returns a new array pointer */
   char *copy_string(const char *source, size_t length) {
+    /* Add 1 to the length to store null-character */
     char *copy = new char[length + 1];
     for (size_t i = 0; i < length; i++) {
       copy[i] = source[i];
@@ -79,57 +86,24 @@ private:
     return copy;
   }
 
-  char *str_;
-  size_t size_;
+  /* Class-scope variables (fields of the class) */
+  char *str_;  /* C-style array containing characters */
+  size_t size_ /* Length of the string excluding null-character */;
 };
-
-/*
-size_t string_len(const char *source) {
-  size_t i = 0;
-  while (source[i] != '\0') {
-    i++;
-  }
-  return i;
-}
-
-void concat_strings() {
-  const char *lhs_str = "beg";
-  const char *rhs_str = "end";
-  size_t lhs_size = string_len(lhs_str);
-  size_t rhs_size = string_len(rhs_str);
-  char *concat = new char[lhs_size + rhs_size + 1];
-  for (size_t i = 0; i < lhs_size; i++) {
-    concat[i] = lhs_str[i];
-  }
-  for (size_t i = 0; i < rhs_size; i++) {
-    concat[lhs_size + i] = rhs_str[i];
-  }
-
-  // do something with concat ...
-
-  delete[] concat;
-}
-
-void concat_oop() {
-  string str_1("hello 1");
-  string str_2("hello 2");
-  string str_3 = str_1 + str_2;
-}
-*/
 
 int main(int argc, char const *argv[]) {
   string str_1("hello 1");
   string str_2("hello 2");
-  string str_3(str_2); // Copy constructor is called here
+  string str_3(str_2); /* Copy constructor is called here */
 
   str_1.debug_print();
   str_2.debug_print();
   str_3.debug_print();
 
-  string str_4;
-  str_4 = str_1; // Asigment operator
-  str_1 = str_1;
+  string str_4 = str_1; /* Copy constructor is called */
+  str_4 = str_1; /* Assignment operator is called */
 
+  /* String concatenation */
   string str_5 = str_1 + str_2;
   str_5.debug_print();
 
